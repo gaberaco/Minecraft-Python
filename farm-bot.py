630990af81a866cb15f27cbfe76a1faf53ad0313
gaberaco <63935301+gaberaco@users.noreply.github.com>
2020-07-24 12:58:48 -0700

    Create farm-bot.py

---
+++ farm-bot.py
@@ -0,0 +1,20 @@
+#Farm Bot
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+mc.postToChat("Farm Bot Active")
+
+
+seeds = 100
+while(seeds > 0):
+    x,y,z = mc.player.getPos()
+    groundBlock = mc.getBlock(x,y-1,z)
+    #check if ground is grass block
+    if groundBlock == 2:
+        mc.setBlock(x,y-1,z,60)
+        mc.setBlock(x,y,z,141)
+        seeds = seeds - 1
+        seedsChat = str(seeds)
+        mc.postToChat("Seed Planted. "+seedsChat+"Seeds Remaining")
+mc.postToChat("FarmBot Deactive")
+
+
